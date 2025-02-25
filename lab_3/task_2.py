99dae41140368b43b001a0f7314e066d3e96e780
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2025-02-25 10:51:51 +0000

    lab_3

--- lab_3/task_2.py
+++ lab_3/task_2.py
@@ -1,9 +1,10 @@
 l1 = [int(i) for i in input("list 1 = ").split()]
 l2 = [int(i) for i in input("list 2 = ").split()]
+res = []
 
-cnt=0
 for i in l1:
-    if i in l2:
-        cnt+=1
-print(cnt)
+    if i in l2 and i not in res:
+        res.append(i)
+        
+print(len(res))
 
